6810bdb7e8d1c1c13480895430a25f80083a0d63
林永璋 <yclin@jumbogames.com.tw>
2018-03-19 18:01:38 +0800

    新增 跑馬燈

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  text = '';
+  otherText = '';
   constructor(public navCtrl: NavController) {
 
+    let text = '';
+    for (let index = 0; index < 20; index++) {
+      text += 'Scrolling text...';
+      text += index;
+    }
+    this.text = text;
   }
-
 }
