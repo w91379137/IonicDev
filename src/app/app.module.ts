2878a6a31e569089cacb8c0ba1c5b4b0f2fbcc3f
林永璋 <yclin@jumbogames.com.tw>
2018-03-19 16:41:38 +0800

    有簡單的 結果 但是 圖片會超過 因為 display: block; z-index; 無效 無法 調整兩個 高度

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ShrinkingSegmentHeaderComponent } from '../components/shrinking-segment-header/shrinking-segment-header';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ShrinkingSegmentHeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Content } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  //相似的結果 shrinking header
+  //https://github.com/ionic-team/ionic/issues/1381
+  //http://codepen.io/kaililleby/pen/HALnJ
+
+
+  //測試實作
+  //https://www.joshmorony.com/creating-a-shrinking-header-for-segments-in-ionic/
+
+  @ViewChild(Content) content: Content;
+  somethings: any = new Array(20);
 
   constructor(public navCtrl: NavController) {
 
